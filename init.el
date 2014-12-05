94ac2fc3c53d35c4c4682dd500b3d5040e959770
Paavo <pparkkin@gmail.com>
2014-12-05 22:57:27 +0900

    Add Ocaml and Haskell modes.

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 (when window-system (set-frame-size (selected-frame) 98 42))
 (setq-default inhibit-startup-screen t)
 (tool-bar-mode -1)
+;(tabbar-mode 1)
+(cua-mode 1)
 (set-face-attribute 'default nil :height 150)
 
 ;;(require 'smooth-scroll)
@@ -16,13 +18,15 @@
 (setq-default column-number-mode t)
 
 (setq backup-directory-alist
-          `((".*" . ,temporary-file-directory)))
-    (setq auto-save-file-name-transforms
-          `((".*" ,temporary-file-directory t)))
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
 
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(setq package-archives '(("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
 (defvar my-packages '(zenburn-theme
@@ -31,7 +35,11 @@
                       paredit
                       highlight-parentheses
                       clojure-mode
-                      clojure-test-mode))
+                      clojure-test-mode
+                      tuareg
+                      utop
+                      merlin
+                      haskell-mode))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
@@ -57,3 +65,15 @@
 (add-hook 'eval-expression-minibuffer-setup-hook
           'lisp-stuff)
 
+(add-hook 'tuareg-mode-hook 'tuareg-imenu-set-imenu)
+(setq auto-mode-alist
+      (append '(("\\.ml[ily]?$" . tuareg-mode)
+                ("\\.topml$" . tuareg-mode))
+              auto-mode-alist)) 
+(autoload 'utop-setup-ocaml-buffer "utop" "Toplevel for OCaml" t)
+(add-hook 'tuareg-mode-hook 'utop-setup-ocaml-buffer)
+(add-hook 'tuareg-mode-hook 'merlin-mode)
+(setq merlin-use-auto-complete-mode t)
+(setq merlin-error-after-save nil)
+
+(add-hook 'haskell-mode-hook 'haskell-indent-mode)
